19f6fc480a98ca73beea4ce88d9f50c2e35fc8b5
Divyareddy <rerohithkumar@gmail.com>
2025-12-30 14:36:14 +0530

    Add files via upload

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,21 +1,31 @@
-public class InsertionSort{
-    public static void main(String[] args)
-    {
-        int[] a={1,4,5,6,10,2};
-        int=a.length;
-        for(int i=0;i<n;i++){
-            int temp=a[i];
-            j=i-1;
-            while(j>=0&&a[j]>temp){
-                a[j+1]=a[j];
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements that are greater than key
+            // one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
-                
             }
-            a[j+1]=temp;
-            for(int i=0;i<n;i++){
-                System.out.println(a[i]+" ");
-            }
-                    }
-        
+
+            // Place key at correct position
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
     }
 }
